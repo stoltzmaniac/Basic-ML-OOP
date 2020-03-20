5fcff2304fbc81f1388fc289c06eefe14a4762ac
Scott Stoltzman <scott@stoltzmanconsulting.com>
2020-03-20 16:35:44 -0600

    Add descriptions

--- 01-Regression/Linear-Regression/Part-02/models/base.py
+++ 01-Regression/Linear-Regression/Part-02/models/base.py
@@ -4,17 +4,22 @@ import pandas as pd
 
 class InputBase:
     def __init__(self):
+        """
+        Base class
+        """
         pass
 
 
-class RawInputData(InputBase):
+class InputData(InputBase):
     def __init__(self, predictor_vars: np.ndarray, response_var: np.ndarray):
         """
+        Houses the raw data. Data should be input as a numpy array, but if it
+        is in a pandas.Dataframe or pandas.Series it will be converted.
         :param predictor_vars: np.ndarray
         :param response_var: np.array (one dimensional)
         """
-        self.predictor_vars = self.convert_to_array(predictor_vars)
-        self.response_var = self.convert_to_array(response_var)
+        self.predictor_vars = self.convert_dataframe_to_array(predictor_vars)
+        self.response_var = self.convert_dataframe_to_array(response_var)
 
         if self.response_var.shape[0] != self.predictor_vars.shape[0]:
             raise ValueError(
@@ -25,7 +30,12 @@ class RawInputData(InputBase):
         super().__init__()
 
     @staticmethod
-    def convert_to_array(df_or_series):
+    def convert_dataframe_to_array(df_or_series):
+        """
+        Converts pandas dataframe to array if necessary
+        :param df_or_series:
+        :return: np.array
+        """
         if isinstance(df_or_series, pd.DataFrame) or isinstance(df_or_series, pd.Series):
             return np.array(df_or_series)
         if isinstance(df_or_series, np.ndarray):
@@ -44,14 +54,25 @@ class RawInputData(InputBase):
                f'-----------------------\n'
 
 
-class SplitTestTrain(RawInputData):
+class SplitTestTrain(InputData):
     def __init__(self, predictor_vars, response_var, train_split=0.70, seed=123):
+        """
+        Split the input data to test / train split to be used in machine learning
+        :param predictor_vars: np.ndarray
+        :param response_var: np.ndarray
+        :param train_split: float percent used as training (Between 0 and 1)
+        :param seed: int for repeatability
+        """
         self.seed = seed
         self.train_split = train_split
         super().__init__(predictor_vars, response_var)
         self.split_data()
 
     def split_data(self):
+        """
+        Separates the data and splits it accordingly, utilizes the random seed
+        :return:
+        """
         np.random.seed(seed=self.seed)
         indices = np.random.permutation(self.predictor_vars.shape[0])
         split_row = round(self.predictor_vars.shape[0] * self.train_split)
